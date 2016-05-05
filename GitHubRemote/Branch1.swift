211f29fde73da1adece75b67ee6d76763fb33d87
Diego Valderrama <skavenger4@hotmail.com>
2016-05-05 08:03:13 -0500

    Done 1st Branch

---
+++ GitHubRemote/Branch1.swift
@@ -0,0 +1,13 @@
+//
+//  Branch1.swift
+//  GitHubRemote
+//
+//  Created by Diego Valderrama on 5/5/16.
+//  Copyright © 2016 Diego Valderrama. All rights reserved.
+//
+
+import Foundation
+
+struct doneBranch1 {
+    
+}
